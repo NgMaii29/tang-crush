d0d2f529f58f527a728b1f986e521f4c326326d4
NgMaii29 <89002630+NgMaii29@users.noreply.github.com>
2021-08-16 15:03:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    introTitle: 'Hee luu cậu!',
+    introDesc: `Tớ có điều này muốn hỏi cậu phải trả lời thật lòng nhaaa`,
+    btnIntro: 'Okee',
+    title: 'Cậu thích tớ phải khum???😙',
+    desc: 'Nếu không trả lời hoặc thoát ra tức cậu là người yêu tớ ',
+    btnYes: 'Ừ,thích lắm <33',
+    btnNo: 'Không, mơ à:3',
+    question:'Nói cho tớ lí do vì sao cậu lại thích tớ điiii <3',
+    btnReply: 'Whyyyy',
+    reply: 'Tại vì cậu dễ thương vãi lìn <33333333',
+    mess: 'Tớ  biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
+    messDesc: 'Nếu đã thích sao không trả lời tin nhắn',
+    btnAccept: 'Okeeee <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
